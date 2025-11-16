5922333952b9b424b045c23bad25dfccf98edd01
tmtuan684 <tuan.tran24@rmit.edu.vn>
2025-11-16 13:40:09 +0700

    update rating review

--- week-04/js-exercises/online-store-w-shopping-cart/checkout.js
+++ week-04/js-exercises/online-store-w-shopping-cart/checkout.js
@@ -9,7 +9,4 @@ function renderOrderSummary() {
 
 document.addEventListener('DOMContentLoaded', async function () {
   renderOrderSummary();
-  updateCartBadge();
-  setupPaymentToggle();
-  setupCheckoutForm();
 });
