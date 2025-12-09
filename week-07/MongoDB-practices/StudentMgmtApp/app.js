48ba9cb1c717c0919600e0b12194d442fbc81eb8
tmtuan684 <tuan.tran24@rmit.edu.vn>
2025-12-09 10:27:40 +0700

    Complete Basic CRUD

--- week-07/MongoDB-practices/StudentMgmtApp/app.js
+++ week-07/MongoDB-practices/StudentMgmtApp/app.js
@@ -6,12 +6,15 @@ const { Student } = require('./model/studentModel');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 (async () => {
     await dbconnect(process.env.CONNECTION_STRING);
     console.log("Connected to DB and models loaded.");
 
     // 2. Define routes AFTER connection
+
+    // Endpoint GET /
     app.get('/', async (req, res) => {
         const students = await Student.find({});
         console.log("Found students:", students.length);
@@ -22,6 +25,66 @@ app.set('view engine', 'ejs');
     app.get('/new-student', (req, res) => {
         res.render('new-student');
     });
+
+    // Endpoint POST /new-student
+    app.post('/new-student', (req, res) => {
+        const newStudent = new Student(req.body);
+
+        newStudent.save()
+            .then((stud) => { 
+                console.log(`New document is saved ${stud}`);
+                res.redirect('/');
+            })
+            .catch((error) => { console.log(error.message)})
+        
+    })
+
+    //Endpoint GET /student/:id/edit
+    app.get('/students/:id/edit', (req, res) => {
+        const studID = req.params.id;
+        Student.findById(studID)
+                .then((student) => {
+                    res.render('edit-student', {student});
+                })
+                .catch((error) => { console.log(error.message)});
+    })
+
+    //Endpoint POST /student/:id/edit 
+    app.post('/students/:id/edit', (req, res) => {
+        const studID = req.params.id;
+        const updates = req.body;
+
+        Student.findByIdAndUpdate(studID, updates, {new: true})
+            .then((student) => {
+                console.log(`Updates ${student}`);
+                res.redirect('/');
+            })
+            .catch((error) => {
+                console.log(error.message);
+            })
+    })
+
+    //Endpoint GET /student/:id/delete
+    app.get('/students/:id/delete', (req, res) => {
+        const studID = req.params.id;
+        Student.findById(studID)
+                .then((student) => {
+                    res.render('delete-student', {student});
+                })
+                .catch((error) => { console.log(error.message)});
+    })
+
+    //Endpoint POST /student/:id/delete 
+    app.post('/students/:id/delete', (req, res) => {
+        const studID = req.params.id;
+
+        Student.findByIdAndDelete(studID)
+                .then((student) => {
+                    console.log(`Delete ${student}`);
+                    res.redirect('/');
+                })
+                .catch((error) => {console.log(error.message)});
+    })
 
     const host = process.env.HOST;
     const port = process.env.PORT || 3000;
