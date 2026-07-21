e7f9456a72e7494b2b7efd97e77f52d7a47b8a58
tmtuan684 <tuan.tran24@rmit.edu.vn>
2026-07-21 22:28:34 +0700

    exercises about JS DOM

---
+++ week-04/js-dom/exercise-5/script/script.js
@@ -0,0 +1,13 @@
+function togglePattern() {
+  // get the pattern
+  var myPattern = document.getElementById('pattern');
+
+  // also get the id of reveal button, so we can change what it says
+  var patternButton = document.getElementById('patternButton');
+
+  // now toggle the pattern and the button text, depending on current state using classList
+
+
+  
+
+}
