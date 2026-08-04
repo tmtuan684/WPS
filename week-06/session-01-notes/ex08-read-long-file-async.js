2b65eb21867b43d5de86e0a3752ce7a852eb8f21
Tuan M. Tran <tuan.tran24@rmit.edu.vn>
2026-08-04 17:55:36 +0700

    minor update

--- week-06/session-01-notes/ex08-read-long-file-async.js
+++ week-06/session-01-notes/ex08-read-long-file-async.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // The next lines of code can be executed even when the file has not completely read.
 fs.readFile('./data/quite-long-text.txt','utf-8', (err, data) => {
     if (err) throw err;
-    console.log('Async read: ', data);    
+    console.log('Async read successfully ');    
 });
 
 console.log('This program reads a file asynchronously.');
