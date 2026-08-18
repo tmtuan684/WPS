b534f9c5cf6ab0a66d590333b7540ee3db7f041f
tmtuan684 <tuan.tran24@rmit.edu.vn>
2026-08-18 22:30:07 +0700

    delete unnecessary files

--- week-08/mongoDB-connection/app.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const db = "DB1";
-const connstr = `mongodb+srv://tuantran24:Blessed@cluster0.lberqlg.mongodb.net/${db}?appName=cluster0`;
-
-// Step 1. Connect to DB
-mongoose.connect(connstr)
-        .then(() => console.log('Connected to MongoDB Atlas'))
-        .catch((err) => console.log(err.message));
--- week-08/mongoDB-connection/dbsupport.js
+++
@@ -1,5 +0,0 @@
-function getConnString(db) {
-    return `mongodb+srv://tmtuan:mypassword@cosc3060.rqcqojz.mongodb.net/${db}?retryWrites=true&w=majority&appName=cosc3060`;
-}
-
-module.exports = { getConnString };
